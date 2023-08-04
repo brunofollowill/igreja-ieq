01e5b698fc2288d3b8220c9e94cd0423c9ef7425
wester <wester@kali>
2023-08-04 17:34:57 -0300

    Sei nem o que fiz

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ import About from "./pages/about";
 
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import Timeline from "./components/timeline";
+import Footer from "./components/footer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <Section />
     <About />
+    <Timeline />
+    <Footer />
     {/* <SingIn /> */}
   </React.StrictMode>
 );
